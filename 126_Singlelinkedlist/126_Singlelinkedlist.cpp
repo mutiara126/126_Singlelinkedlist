9ef8cce832fd3d4d75783af42378cae224d3ff62
mutiara126 <salsamutiara1812@gmail.com>
2023-05-18 19:08:10 +0800

    untuk menampilkan nim yang sudah ada

--- 126_Singlelinkedlist/126_Singlelinkedlist.cpp
+++ 126_Singlelinkedlist/126_Singlelinkedlist.cpp
@@ -31,6 +31,19 @@ void addNode() {
 		return;
 
 	}
+    
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "Nim sudah ada" << endl;
+			return;
+
+		}
+	}
 
 
 
